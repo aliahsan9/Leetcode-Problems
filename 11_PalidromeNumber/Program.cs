8cf9b0d5bfefdade7814bd8a6841cff0ab2b318c
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-14 23:42:49 -0700

    Palidrome Number

---
+++ 11_PalidromeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any value to check palidrome:");
+        int x = int.Parse(Console.ReadLine());
+
+        if (x < 0)
+        {
+            Console.WriteLine("It's not a palidrome!");
+        }
+
+        int original = x, reversed = 0;
+
+        while (x > 0)
+        {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        if (original == reversed)
+        {
+            Console.WriteLine("It's a palidrome number.");
+        }
+        else
+        {
+            Console.WriteLine("It's not a palidrome number!");
+        }
+    }
+}
