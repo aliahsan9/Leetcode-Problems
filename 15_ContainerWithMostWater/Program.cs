8e6dcfa937c67d15cccdef9bc43c75e063e8aba2
Ali Ahsan <aliahsan8751@gmail.com>
2025-08-16 01:26:25 -0700

    Container with most water leetcode problem using Two pointer approach with best time complexity

---
+++ 15_ContainerWithMostWater/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static int MaxArea(int[] height)
+    {
+        int leftPointer = 0, rightPointer = height.Length - 1;
+        int maxWater = 0;
+
+        while (leftPointer < rightPointer)
+        {
+            int minHeight = Math.Min(height[leftPointer], height[rightPointer]);
+            int width = rightPointer - leftPointer;
+            int area = minHeight * width;
+
+            maxWater = Math.Max(area, maxWater);
+
+            if (height[leftPointer] < height[rightPointer])
+                leftPointer++;
+            else
+                rightPointer--;
+        }
+        return maxWater;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = MaxArea(height);
+        Console.WriteLine("Maximum water is: " + result);
+    }
+}
